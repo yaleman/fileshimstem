5ac23ded12c97a7700014d3c545146277efece1b
James Hodgkinson <james@terminaloutcomes.com>
2022-01-06 21:58:30 +1000

    logging

--- fileshimstem.py
+++ fileshimstem.py
@@ -44,6 +44,7 @@ def show_subpath(subpath):
 
     if fullpath.is_file:
         if request.method == "HEAD":
+            app.logger.debug("HEAD")
             stat = fullpath.stat()
             data = {
                 "type" : "file",
@@ -53,6 +54,7 @@ def show_subpath(subpath):
                     data[attr.lstrip("st_")] = getattr(stat, attr)
             return json.dumps(data, default=str, indent=4)
         elif request.method == "GET":
+            app.logger.debug("GET")
             return send_file(fullpath)
     abort(500)
 
